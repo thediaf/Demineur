05b3bf092252805063a964aa129480af9a2a16cd
thediaf <jaharacs98@gmail.com>
2022-04-18 17:48:20 +0000

    A test class create

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		
+		window.setTitle("Jeu de demineur");
+		
+		window.setSize(400, 300);
+		
+		window.setLocationRelativeTo(null);
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.setVisible(true);
+	}
+
+}
