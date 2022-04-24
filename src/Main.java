06e51218151f892e80def06faf7cdbcdd1e7f87c
thediaf <jaharacs98@gmail.com>
2022-04-24 22:28:25 +0000

    feat: level options pane

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
+		int toughness = 1;
+        Object[] options = {"Débutant", "Intermédiaire", "Difficile"};
+        toughness = JOptionPane.showOptionDialog(null,
+                "Quel est votre niveau de difficulite ?", "Niveau",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+        if(toughness == -1)
+            System.exit(0);
 		Game game = new Game(8);
 		game.mainWindow(game, 8);
 	}
